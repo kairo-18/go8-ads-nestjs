cb1164a035ee9ac9e41dba7d4b9df5a65d0b8e1b
kairo-18 <gladimirdungo@gmail.com>
2025-02-19 23:44:24 +0800

    global prefix added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://152.42.176.184', 'http://localhost:5173'],
+    origin: ['http://152.42.176.184', 'http://localhost:5173', 'http://localhost:3000'],
   });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
